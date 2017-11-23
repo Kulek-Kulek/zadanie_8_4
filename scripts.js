ca3d25eb57cb553863868070669adc010704a324
Krzysztof Ługowski <k.lugowski@yahoo.com>
2017-11-23 15:28:08 +0100

    Add a new file

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// alert('Hello, this is my linear comment no 1');
+
+var name = prompt('Enter your name');
+/*
+console.log('Hello again. This is my block comment. The commented will not be called');
+*/
+alert('Hello, ' + name);
+// and this is my second linear comment
+console.log('Hello, ' + name);
